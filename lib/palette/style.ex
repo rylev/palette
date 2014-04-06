5364b53686e77448b126920abefa4bd2b9f5ebb7
Ryan Levick <ryan@6wunderkinder.com>
2014-04-06 23:51:29 +0200

    parse rgb

--- lib/palette/style.ex
+++ lib/palette/style.ex
@@ -1,4 +1,9 @@
 defmodule Palette.Style do
+  def fg(string, color) do
+    color = color_approximation(color)
+    process string, &(IO.ANSI.escape "%{#{color}}#{&1}")
+  end
+
   def bright(string) do
     process string, &(IO.ANSI.escape "%{bright}#{&1}")
   end
@@ -9,5 +14,46 @@ defmodule Palette.Style do
 
   defp process(string, process_fun) do
     String.split(string, "\n") |> Enum.filter(&(&1 != "")) |> Enum.map(process_fun) |> Enum.join("\n")
+  end
+
+  defp color_approximation(rgb_string) do
+    parse_rgb rgb_string
+    "red"
+  end
+
+  defp parse_rgb(rgb_string) when is_binary rgb_string do
+    unless valid_rgb_string?(rgb_string) do
+      raise "#{rgb_string} is not a valid rgb color"
+    end
+
+    rgb_string |>
+    rgb_hex |>
+    String.split("") |>
+    Enum.filter(&(&1 != "")) |>
+    fn list -> Enum.chunk(list, div(Enum.count(list), 3)) end.() |>
+    Enum.map(&Enum.join/1) |>
+    Enum.map(&(binary_to_integer &1, 16))
+  end
+
+  def valid_rgb_string?(rgb_string) do
+    valid_rgb_format?(rgb_string) && valid_rgb_length?(rgb_string)
+  end
+
+  def rgb_hex(rgb_string) do
+    case rgb_string do
+      "#" <> rest -> rest
+      "0x" <> rest -> rest
+      _ -> rgb_string
+    end
+  end
+
+  defp valid_rgb_length?(rgb_string) do
+    hex = rgb_hex rgb_string
+    String.length(hex) == 6 || String.length(hex) == 3
+  end
+
+  defp valid_rgb_format?(rgb_string) do
+    rgb_regexp = ~r/^(0x|#)?([0-9a-fA-F][0-9a-fA-F]?){3}$/
+    String.match? rgb_string, rgb_regexp
   end
 end
--- test/palette/style_test.exs
+++ test/palette/style_test.exs
@@ -8,6 +8,11 @@ defmodule StyleTest do
     """
   end
 
+  test "fg/2 colors the foreground of each line of the passed in string" do
+    assert Palette.Style.fg(string, "#ff0000") == "\e[38;5;196mThis is the first line.\e[0m\n\e[38;5;196mAnd this is the second.\e[0m"
+    assert Palette.Style.fg(string, "#2d3091") == "\e[38;5;19mThis is the first line.\e[0m\n\e[38;5;19mAnd this is the second.\e[0m"
+  end
+
   test "bright/1 brightens each line of the passed in string" do
     assert Palette.Style.bright(string) == "\e[1mThis is the first line.\e[0m\n\e[1mAnd this is the second.\e[0m"
   end
