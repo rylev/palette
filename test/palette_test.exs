3367ffb4de5b662b723799eb16ae0386e9d2948f
Ryan Levick <ryan@6wunderkinder.com>
2014-04-11 23:15:17 +0200

    update tests

--- test/palette_test.exs
+++ test/palette_test.exs
@@ -12,11 +12,24 @@ defmodule PaletteTest do
     assert Palette.fg(string, "#2d3091") == "\e[38;5;19mThis is the first line.\e[0m\n\e[38;5;19mAnd this is the second.\e[0m"
   end
 
+  test "bg/2 colors the background of each line of the passed in string" do
+    assert Palette.bg(string, "#ff0000") == "\e[48;5;196mThis is the first line.\e[0m\n\e[48;5;196mAnd this is the second.\e[0m"
+    assert Palette.bg(string, "#2d3091") == "\e[48;5;19mThis is the first line.\e[0m\n\e[48;5;19mAnd this is the second.\e[0m"
+  end
+
   test "bright/1 brightens each line of the passed in string" do
     assert Palette.bright(string) == "\e[1mThis is the first line.\e[0m\n\e[1mAnd this is the second.\e[0m"
   end
 
+  test "bold/1 boldens each line of the passed in string" do
+    assert Palette.bold(string) == "\e[1mThis is the first line.\e[0m\n\e[1mAnd this is the second.\e[0m"
+  end
+
   test "underline/1 underlines each line of the passed in string" do
     assert Palette.underline(string) == "\e[4mThis is the first line.\e[0m\n\e[4mAnd this is the second.\e[0m"
+  end
+
+  test "inverse/ inverses the background and foreground colors" do
+    assert Palette.inverse(string) == "\e[7mThis is the first line.\e[0m\n\e[7mAnd this is the second.\e[0m"
   end
 end
